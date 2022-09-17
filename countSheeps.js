49b1944b7850d4ee99b017b036f1c2f4c43f63df
Lukasz Wargocki <lmwargocki@gmail.com>
2022-09-17 22:32:36 +0200

    return number of true values in an array(mind the nulls)

--- countSheeps.js
+++ countSheeps.js
@@ -2,6 +2,5 @@
 We need a function that counts the number of sheep present in the array (true means present).
 */
 function countSheeps(arrayOfSheep) {
-  return arrayOfSheep.reduce((acc, item) => acc + item, 0); 
-  // and to think people complain on funky js type conversions
+  return arrayOfSheep.filter(item => !!item).length; 
 }
