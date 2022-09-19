807b86bef334e441c9e3bffc691bb52f9d038a3d
Lukasz Wargocki <lmwargocki@gmail.com>
2022-09-19 18:15:09 +0200

    given an input of three integers, return their suml

---
+++ goals.js
@@ -0,0 +1,5 @@
+// given an input of three integers, return their sum
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
