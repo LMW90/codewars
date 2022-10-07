def840402ce639de4cb6d319690897dec3fe9f19
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-07 22:11:45 +0200

    done

---
+++ welcome.js
@@ -0,0 +1,28 @@
+// return a welcome message depending on an input language
+
+function greet(language) {
+	if (language in table){
+    return table[language]
+  }
+  return table["english"]
+}
+
+var table = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+}
