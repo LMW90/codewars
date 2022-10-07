2e1dbaef545d2b19790f3c48f4db20677adc91cb
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-07 21:59:10 +0200

    return third inner angle of a triangle

---
+++ thirdAngleOfATriangle.js
@@ -0,0 +1,4 @@
+// return third inner angle of a triangle, given it's two other angles
+function otherAngle(a, b) { // input value passed as a number of degrees number data type
+  return 180 - a - b; // return value as a number representing number of degrees
+}
