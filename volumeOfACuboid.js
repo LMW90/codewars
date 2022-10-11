ed0f7f46dafc805b207778d4d2ac06bb73c9d4b3
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-11 19:00:31 +0200

    write a static method that, given it's dimensions, calculates a volume of a cuboid

---
+++ volumeOfACuboid.js
@@ -0,0 +1,6 @@
+// write a static method that, given it's dimensions, calculates a volume of a cuboid
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height
+  }
+}
