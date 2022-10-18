55d22c66e0055b3f8568538b6ceee5857407d572
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-18 11:46:36 +0200

    fixed the buggy code

---
+++ dalmatiansSquashTheBugsNotTheDogs.js
@@ -0,0 +1,13 @@
+// fix the buggy code
+function howManyDalmatians(number){
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"]
+  console.log(number)
+  var respond = number <= 10 
+    ? dogs[0]
+    : (number <= 50) 
+      ? dogs[1] 
+      : (number == 101)
+        ? dogs[3] 
+        : dogs[2]
+  return respond
+}
