6ccafc0fb2dfbf3b26281f397d526479f01756f2
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-25 18:20:03 +0200

    reverse every other word in a string

---
+++ 6thKyu/reverseEveryOtherWordInTheString.js
@@ -0,0 +1,10 @@
+/* Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+function reverse(str){
+  return str = str.split(' ').map((element, index)=>{
+    if (index %2 == 1) element = element.split('').reverse().join('')
+    return element
+  })
+    .join(' ')
+    .trim()
+}
