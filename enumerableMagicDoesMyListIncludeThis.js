dd962aea522fecde6a63d6a878e03e3c15f60924
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-30 08:42:03 +0100

    create a method that determines if input array includes input item

---
+++ enumerableMagicDoesMyListIncludeThis.js
@@ -0,0 +1,5 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+function include(arr, item){
+  // return whether arr includes item or not
+  return arr.includes(item)
+}
