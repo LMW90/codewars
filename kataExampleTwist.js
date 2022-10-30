3972dbca277c3357ec76a8b8113e7e810e35f550
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-30 08:39:01 +0100

    add value "codewars" to a websites array thousand times

---
+++ kataExampleTwist.js
@@ -0,0 +1,4 @@
+//This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata). 
+// add the value "codewars" to the websites array 1,000 times
+// create an empty 1000 items array and fill it with codewars
+let websites = Array(1000).fill('codewars')
