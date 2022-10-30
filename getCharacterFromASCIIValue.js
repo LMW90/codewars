db25bb7ae4df62ec1e27a2d8711eb46e110c441b
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-30 08:22:26 +0100

    given an integer, return a character with corresponding ASCII code

---
+++ getCharacterFromASCIIValue.js
@@ -0,0 +1,14 @@
+/* Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65)
+
+should return:
+
+'A'
+*/
+function getChar(c){
+  // return a string from input char code
+  return String.fromCharCode(c)
+}
