5653a4aa16ccb2367c94814f73cfbf1864e6ba44
Lukasz Wargocki <lmwargocki@gmail.com>
2022-10-31 12:50:17 +0100

    given a string with a digit in each word, return a string with words ordered according to those digits

---
+++ 6thKyu/yourOrderPlease.js
@@ -0,0 +1,21 @@
+/* Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+Examples
+
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+function order(words){
+  // split string into array of words
+  words = words.split(' ')
+  // find a digit in each word and sort according to them
+  words.sort((a, b) => {
+   return (a.match(/[0-9]/g)[0] - b.match(/[0-9]/g)[0])
+  })
+  // join string back and return
+  return words.join(' ')
+}
