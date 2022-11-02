626515d47c60d86c5141e4fb191a1c5c9a0e9c56
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-02 18:36:06 +0100

    make a score of functions that, given two numbers, return product of mathematical operation with input as operands

---
+++ fundamentalsReturn.js
@@ -0,0 +1,24 @@
+// make a score of functions that, given two numbers, return product of mathematical operation with input as operands
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return a ** b
+}
+  
+function subt(a,b){
+  return a - b
+}
