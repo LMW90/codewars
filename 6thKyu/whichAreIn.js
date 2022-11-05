8549e91cd7a02d1f517c643ea3c8947b1d4aabdc
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-05 23:17:05 +0100

    given two arrays, return first array"s items that are substring of any of other array"s element

---
+++ 6thKyu/whichAreIn.js
@@ -0,0 +1,25 @@
+/* Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+Example 1:
+
+a1 = ["arp", "live", "strong"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns ["arp", "live", "strong"]
+Example 2:
+
+a1 = ["tarp", "mice", "bull"]
+
+a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+returns []
+Notes:
+
+    Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+    In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+    Beware: In some languages r must be without duplicates.
+*/
+function inArray(array1,array2){
+  // filter in arr1 items that are included in any element of arr2 and default sort them
+  return array1.filter((item)=>array2.some(element => element.includes(item))).sort()
+}
