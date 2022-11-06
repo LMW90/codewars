d6e04183eadc7c7862e8946a49b50a2985e2fb15
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-06 12:07:58 +0100

    create a static method that prints hello world into the console

---
+++ classicHelloWorld.js
@@ -0,0 +1,15 @@
+/* You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+Note that for some languages, the function main is the entry point of the program.
+
+Here's how it will be tested:
+
+    Solution.main("parameter1","parameter2");
+*/    
+// Print "Hello World!" to the screen
+class Solution {
+  // create a static method that prints hello world
+  static main(){
+    console.log("Hello World!")
+  }
+}
