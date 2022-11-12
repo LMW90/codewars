65a0df56b07aa635a37c5645cad5267cf6408c5e
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-12 14:27:31 +0100

    given a string return pairs of its characters with last pair as character + dash if string is odd

---
+++ 6thKyu/splitStrings.js
@@ -0,0 +1,21 @@
+/* Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+*/
+function solution(str){
+  // handle zero and one character strings
+  if (!str) return []
+  if (str.length === 1) return '_' + str
+  // create array of pairs of characters
+  let result = str.match(/.{2}/g)
+  // of string length is odd, add last character with a dash
+  if (str.length % 2 === 1) {
+    let suffix = str.match(/.$/)[0]
+    result.push(`${suffix}_`)
+  }
+  // return the array
+  return result
+}
