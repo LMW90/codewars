873c4f541b60ad52ccb3a9bb960217739ed72459
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-13 17:35:26 +0100

    / fix the closure issue

---
+++ shiftyClosures.js
@@ -0,0 +1,10 @@
+// fix the closure issue
+function greet_abe() {
+  let name = 'Abe';
+  return "Hello, " + name + '!'
+}
+
+function greet_ben() {
+  let name = 'Ben'
+  return "Hello, " + name + '!'
+}
