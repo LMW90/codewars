6ed6fb0dd935fb6313d74cda765f58b9fdc38290
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-18 09:51:12 +0100

    given a carthesian coordinates of two point, return coordinates of a point symmetric to first point about second point

---
+++ pointsOfReflection.js
@@ -0,0 +1,8 @@
+/* Given two points P and Q, output the symmetric point of point P about Q. 
+Each argument is a two-element array of integers representing the point's X and Y coordinates. 
+Output should be in the same format, giving the X and Y coordinates of point P1. 
+You do not have to validate the input.
+*/
+function symmetricPoint(p, q) {
+  return [q[0] - (p[0]-q[0]), q[1] - (p[1]-q[1])]
+}
