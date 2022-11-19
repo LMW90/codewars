ed447157eb59605641176c2466c3effd4251e41f
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-19 10:21:50 +0100

    create an object with three object properties

---
+++ grasshopper-createRooms.js
@@ -0,0 +1,22 @@
+/* Escape the room
+
+You are creating an "Escape the room" game. The first step is to create a hash table called rooms that contains all of the rooms of the game. There should be at least 3 rooms inside it, each being a hash table with at least three properties (e.g. name, description, completed).
+*/
+var rooms = {
+  "red room": {
+    "name": "red rum",
+    "descripion": "barbiturate overdose suicide site",
+    "completed": false
+  },
+  "tricycle corridor": {
+    "name": "tricycle corridor",
+    "description": "narrow escape from possessed tricylce",
+    'completed': true
+  },
+  "who cares": {
+    "name": "who cares",
+    "description": "escape from masked killer among indifferent crowd",
+    "completed": false
+    
+  }
+}
