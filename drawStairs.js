78aeaad3c8056b49119921a65b3a4cf5dd926638
Lukasz Wargocki <lmwargocki@gmail.com>
2022-11-21 10:50:49 +0100

    given an amount of steps, draw stairs;

---
+++ drawStairs.js
@@ -0,0 +1,19 @@
+/* Given a number n, draw stairs using the letter "I", n tall and n wide, with the tallest in the top left.
+
+For example n = 3 result in:
+
+"I\n I\n  I"
+
+or printed:
+
+I
+ I
+  I
+*/
+function drawStairs(n) {
+  let stairs = []
+  for (let i = 0; i < n; i++){
+    stairs.push(`${' '.repeat(i)}I`)
+  }
+  return stairs.join('\n')
+}
