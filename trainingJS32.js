c4259ec9466cd06cb1025220cc77457f9cb58cf3
Lukasz Wargocki <lmwargocki@gmail.com>
2022-12-01 13:24:43 +0100

    depending on the position of dot relative to a length of an input number, return rounded, ceiled or floored integer

---
+++ trainingJS32.js
@@ -0,0 +1,20 @@
+/* Coding in function roundIt. function accept 1 parameter n. It's a number with a decimal point. Please use different methods based on the location of the decimal point, turn the number into an integer.
+
+If the decimal point is on the left side of the number (that is, the count of digits on the left of the decimal point is less than that on the right), Using ceil() method.
+
+roundIt(3.45) should return 4
+
+If the decimal point is on the right side of the number (that is, the count of digits on the left of the decimal point is more than that on the right), Using floor() method.
+
+roundIt(34.5) should return 34
+
+If the decimal point is on the middle of the number (that is, the count of digits on the left of the decimal point is equals that on the right), Using round() method.
+
+roundIt(34.56) should return 35
+*/
+function roundIt(n){
+  let string = String(n)
+  let len = string.length
+  let dotIndex = string.indexOf('.')
+  return (dotIndex + 1<= len/2)? Math.ceil(n) : (dotIndex >= len/2) ? Math.floor(n) : Math.round(n)
+}
