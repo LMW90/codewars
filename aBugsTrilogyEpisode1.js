f129064ae078cc0f14fdb34a5f3dc58406dd7bce
Lukasz Wargocki <lmwargocki@gmail.com>
2022-12-18 20:57:59 +0100

    fix the buggy code

---
+++ aBugsTrilogyEpisode1.js
@@ -0,0 +1,9 @@
+// fix the bugs in the code
+function yourFutureCareer(){
+	const career = Math.random()
+  if (career <= 0.32) {return 'FrontEnd Developer'}
+  else if (career <= 0.65) {return 'BackEnd Developer'}
+  else {return 'Full-Stack Developer'}
+}
+
+yourFutureCareer();
