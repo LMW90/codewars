8f16dfa41dc31b3ae37a3484597f4d65a507becc
Lukasz Wargocki <lmwargocki@gmail.com>
2022-12-22 15:18:38 +0100

    given a string and array of integers, return a string after repetitively removing characters with integer index in a string

---
+++ 7thKyu/lastSurvivor.js
@@ -0,0 +1,27 @@
+/* You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+
+Notes
+
+    The given string will never be empty.
+    The length of the array is always one less than the length of the string.
+    All numbers are valid.
+    There can be duplicate letters and numbers.
+*/
+function lastSurvivor(letters, coords) {
+  // for each coordinate in sequence
+  for (let coord of coords){
+    // reassign to string it's part before coord char and after coord char
+    letters = letters.slice(0, coord) + letters.slice(coord+1, letters.length);
+  }
+  return letters;
+}
