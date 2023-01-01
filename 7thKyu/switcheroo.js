f93866af034377e3276d9a9cba094f8fb18ac979
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-01 16:11:15 +0100

    given a string of "a"s, "b"s and "c"s, return a given string with as and bs swapped

---
+++ 7thKyu/switcheroo.js
@@ -0,0 +1,20 @@
+/* iven a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+function switcheroo(x){
+  let newStr = ''
+  for (let char of x){
+    if (char == 'a'){
+      newStr += 'b';
+    } else if (char == 'b'){
+      newStr += 'a';
+    } else {
+      newStr += char;
+    }
+  }
+  return newStr;
+}
