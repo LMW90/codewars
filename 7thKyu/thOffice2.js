bbfc71cad62a90aef05e0aade7d7302c2a2b31c9
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-06 16:07:36 +0100

    Michael Scott for president

---
+++ 7thKyu/thOffice2.js
@@ -0,0 +1,29 @@
+/* Every now and then people in the office moves teams or departments. Depending what people are doing with their time they can become more or less boring. Time to assess the current team.
+
+You will be provided with an object(staff) containing the staff names as keys, and the department they work in as values.
+
+Each department has a different boredom assessment score, as follows:
+
+accounts = 1
+finance = 2
+canteen = 10
+regulation = 3
+trading = 6
+change = 6
+IS = 8
+retail = 5
+cleaning = 4
+pissing about = 25
+
+Depending on the cumulative score of the team, return the appropriate sentiment:
+
+<=80: 'kill me now'
+< 100 & > 80: 'i can handle this'
+100 or over: 'party time!!'
+*/
+function boredom(staff){
+  let depts = {"accounts": 1, "finance": 2, "canteen": 10, "regulation": 3, "trading": 6,
+                  "change": 6, "IS": 8, "retail": 5, "cleaning": 4, "pissing about": 25}
+  let boreScore = Object.values(staff).reduce((score,dept) => score + depts[dept], 0);
+  return (boreScore > 100)? "party time!!" : (boreScore > 80)? "i can handle this" : "kill me now";
+}
