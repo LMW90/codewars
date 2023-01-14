62b341be6d14ce80d8a0ec942527122c82bba419
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-14 15:27:49 +0100

    given an alphanumeric string, return biggest contiguous number within it

---
+++ 7thKyu/numbersInStrings.js
@@ -0,0 +1,10 @@
+/* In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number. Numbers will not have leading zeros.
+
+For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+Good luck!
+*/
+function solve(s){
+  // retrun bigest among numbers found within a string
+  return Math.max(...s.match(/\d+/g));
+};
