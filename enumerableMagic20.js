5d094c70eecf49d26fc5c89eb9f76f8ac137d37a
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-25 18:21:44 +0100

    given an array and an integer, return an array of arrays of sacading integer-sized subsets of an array

---
+++ enumerableMagic20.js
@@ -0,0 +1,20 @@
+/* Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+each_cons([1,2,3,4], 2)
+  #=> [[1,2], [2,3], [3,4]]
+
+each_cons([1,2,3,4], 3)
+  #=> [[1,2,3],[2,3,4]]
+  
+
+As you can see, the lists are cascading; ie, they overlap, but never out of order.
+*/
+function eachCons(array, n) {
+  if (n == 1) return array.map(item => [item]);
+  let cascades = [];
+  for (let i = 0; i < array.length; i++){
+    let item = array.slice(i, (i+n));
+    if (item.length === n) cascades.push(item);
+  }
+	return cascades;
+}
