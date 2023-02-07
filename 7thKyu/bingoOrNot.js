69640861ef415c345ead6e20020c54108d84a14f
Łukasz Wargocki <lmwargocki@gmail.com>
2023-02-07 14:52:23 +0100

    given an input array of integers, return "WIN" if numeric position in alphabet of every character in word "bingo" is present in the array, "LOSE" otherwise

---
+++ 7thKyu/bingoOrNot.js
@@ -0,0 +1,10 @@
+/* For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
+
+Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+*/
+function bingo(a) {
+  // determine numbers for characters in bingo
+  const codes = 'BINGO'.split('').map(char => char.charCodeAt() - 64);
+  // return depending on every 'bingo' number being present in the input
+  return codes.every(code => a.includes(code)) ? "WIN" : "LOSE";
+}
