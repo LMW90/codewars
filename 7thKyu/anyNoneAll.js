a4f218c8e1c75e85b6287c9e70c74cfda02c66fe
Lukasz Wargocki <lmwargocki@gmail.com>
2023-02-23 11:54:27 +0100

    implements methods all, none and any for array prototype without using array methods

---
+++ 7thKyu/anyNoneAll.js
@@ -0,0 +1,39 @@
+/* As a part of this Kata, you need to create three functions that one needs to be able to call upon an array:
+
+    all
+
+    This function returns true only if the predicate supplied returns true for all the items in the array [1, 2, 3].all(isGreaterThanZero) => true [-1, 0, 2].all(isGreaterThanZero) => false
+
+    none
+
+    This function returns true only if the predicate supplied returns false for all the items in the array [-1, 2, 3].none(isLessThanZero) => false [-1, -2, -3].none(isGreaterThanZero) => true
+
+    any
+
+    This function returns true if at least one of the items in the array returns true for the predicate supplied [-1, 2, 3].any(isGreaterThanZero) => true [-1, -2, -3].any(isGreaterThanZero) => false
+
+You do not need to worry about the data supplied, it will be an array at all times.
+*/
+Array.prototype.all = function (p) {
+  // return false if predicate returns false for any of items, true otherwise
+  for (let element of this) {
+    if (!p(element)) return false;
+  }
+  return true;
+};
+
+Array.prototype.none = function (p) {
+  // return false if predicate returns true for any of items, true otherwise
+  for (let element of this) {
+    if (p(element)) return false;
+  }
+  return true;
+};
+
+Array.prototype.any = function (p) {
+  // return true if predicate returns true for any of items, fasle otherwise
+  for (let element of this) {
+    if (p(element)) return true;
+  }
+  return false;
+};
