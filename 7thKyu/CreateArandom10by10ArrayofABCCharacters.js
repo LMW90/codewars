57170414e9ca0c463d84b95dde66674c787d9316
Łukasz Wargocki <lmwargocki@gmail.com>
2023-03-21 23:42:55 +0100

    create a 10x10 2D array of randomsingle  "A"s, "B"s and "C"s in each cell

--- 7thKyu/CreateArandom10by10ArrayofABCCharacters.js
+++ 7thKyu/CreateArandom10by10ArrayofABCCharacters.js
@@ -1,8 +1,19 @@
 /* Create 10x10 array and randomly fill each cell with 'A', 'B', 'C' or 'D' and ensure that each row contain at least one occurance of each letter. Example: [ ['A', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['A', 'B', 'B', 'C', 'B', 'D', 'A', 'A', 'C', 'D'], ['A', 'A', 'A', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['C', 'A', 'B', 'C', 'B', 'C', 'D', 'B', 'C', 'A'], ['A', 'A', 'B', 'D', 'B', 'B', 'A', 'C', 'B', 'A'], ['B', 'A', 'B', 'C', 'B', 'D', 'A', 'B', 'C', 'B'], ['A', 'A', 'D', 'C', 'A', 'D', 'B', 'C', 'C', 'C'], ['B', 'A', 'B', 'C', 'B', 'D', 'B', 'C', 'C', 'D'], ['D', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['A', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ]
 */
 function array10(){
+  // built array
+  const array = [];
   // characters to pull from
-  let chars = ['A','B','C']
-  // create a ten item sparse array and fill it with ten random chars arrays
-	return Array.from()
+  const chars = ['A','B','C','D'];
+    // run until 10 subarrays
+    for (let i = 0; array.length < 10; i++){
+      let temp = [];
+      // generate random subarray
+      for (let j = 0; j < 10; j++){
+        temp.push(chars[Math.floor(Math.random()*4)]);
+      }
+      // append only subarrays with at least one of each characters
+      if (chars.every(char => temp.includes(char))) array.push(temp);
+    }
+  return array;
 }
