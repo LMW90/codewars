0518dd812701a21609ada1e242e48d7fc7c5a4e6
Łukasz Wargocki <lmwargocki@gmail.com>
2023-03-21 15:03:19 +0100

    create a 10x10 2D array of randomsingle  "A"s, "B"s and "C"s in each cell

---
+++ 7thKyu/CreateArandom10by10ArrayofABCCharacters.js
@@ -0,0 +1,9 @@
+/* Create 10x10 array and randomly fill each cell with 'A', 'B', 'C' or 'D' and ensure that each row contain at least one occurance of each letter. Example: [ ['A', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['A', 'B', 'B', 'C', 'B', 'D', 'A', 'A', 'C', 'D'], ['A', 'A', 'A', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['C', 'A', 'B', 'C', 'B', 'C', 'D', 'B', 'C', 'A'], ['A', 'A', 'B', 'D', 'B', 'B', 'A', 'C', 'B', 'A'], ['B', 'A', 'B', 'C', 'B', 'D', 'A', 'B', 'C', 'B'], ['A', 'A', 'D', 'C', 'A', 'D', 'B', 'C', 'C', 'C'], ['B', 'A', 'B', 'C', 'B', 'D', 'B', 'C', 'C', 'D'], ['D', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ['A', 'A', 'B', 'C', 'B', 'D', 'A', 'C', 'C', 'D'], ]
+*/
+function array10(){
+  // characters to pull from
+  let chars = ['A','B','C']
+  // create a ten item sparse array and fill it with ten random chars arrays
+	return Array(10)
+    .fill(Array.from({length: 10}, (_k,_v) => chars[Math.floor(Math.random()*(3))]));
+}
