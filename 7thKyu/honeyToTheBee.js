ee2b4a1acde57097271cf82903853222717fe1ce
Lukasz Wargocki <lmwargocki@gmail.com>
2023-04-14 12:54:58 +0200

    find out why Bee.prototype.unloadPollen doesnt work properly

---
+++ 7thKyu/honeyToTheBee.js
@@ -0,0 +1,21 @@
+// Find out why the amount of honey in the hive is not increasing.
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+function Hive() {
+  this.pollen = 100;
+}
+
+Hive.prototype.addPollen = function(pollen) {
+  this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+  return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+  // missing this @ hive
+  this.hive.addPollen(this.capacity);
+}
