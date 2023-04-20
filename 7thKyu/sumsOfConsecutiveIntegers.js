9af839301b2284b0507477133ba1bd7075b69e42
Łukasz Wargocki <lmwargocki@gmail.com>
2023-04-20 13:06:25 +0200

    Given the number of consecutive integers and the total of the integers, return the consecutive integer at the requested position.

---
+++ 7thKyu/sumsOfConsecutiveIntegers.js
@@ -0,0 +1,15 @@
+/* The sum of x consecutive integers is y. What is the consecutive integer at position n? Given x, y, and n, solve for the integer. Assume the starting position is 0.
+
+For example, if the sum of 4 consecutive integers is 14, what is the consecutive integer at position 3?
+
+We find that the consecutive integers are [2, 3, 4, 5], so the integer at position 3 is 5.
+
+position(4, 14, 3) == 5
+
+Assume there will always be a sum of x consecutive integers that totals to y and n will never be indexed out of bounds.
+*/
+function position(x, y, n) {
+  // convert the eqution for sum of consecutive integers
+  let firstInteger = y/x -x/2 +1/2;
+  return firstInteger + n;
+}
