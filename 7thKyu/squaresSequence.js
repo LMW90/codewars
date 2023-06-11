20d18ffc2ce4b0daaa1e1f3422cc65c07c07e4a9
Lukasz Wargocki <lmwargocki@gmail.com>
2023-06-11 17:20:09 +0200

    given two integers x represencting first element of a sequence and n representing length of a sequence, return an array of numbers being the sequence of each number being square of previous number

---
+++ 7thKyu/squaresSequence.js
@@ -0,0 +1,10 @@
+/* Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+Examples
+
+2, 5  -->  [2, 4, 16, 256, 65536]
+3, 3  -->  [3, 9, 81]
+*/
+function squares(x, n) {
+  // build an n-lenght array with it's elements being a geometric sequence of x
+  return Array.from({ length: n }, (_,key) => x**(2**key));
+}
