11654a3f89124d11480b06ac6301f0cceb13ff5c
Lukasz Wargocki <lmwargocki@gmail.com>
2023-06-11 17:39:19 +0200

    debug function imporperly summing integers" digits (wrong indexing in for loop)

---
+++ 7thKyu/debugSumOfDigitsOfANumber.js
@@ -0,0 +1,15 @@
+/* Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+Example
+
+123  => 6
+223  => 7
+1337 => 14
+*/
+function getSumOfDigits(integer) {
+  var sum = null;
+  var digits =  integer.toString();
+  for(var i = 0; i < digits.length; i++) {
+    sum += +digits[i];
+  }
+  return sum;
+}
