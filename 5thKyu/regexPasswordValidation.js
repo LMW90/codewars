472d005dcb0337ecb3e55ee8734ce0210baea9c2
Lukasz Wargocki <lmwargocki@gmail.com>
2023-06-16 16:09:35 +0200

    write a regular expression that matches at least 6 characters long, at least one upper case letter, at least one lower case letter at least one digit alphanumeric strings

---
+++ 5thKyu/regexPasswordValidation.js
@@ -0,0 +1,10 @@
+/* You need to write regex that will validate a password to make sure it meets the following criteria:
+
+    At least six characters long
+    contains a lowercase letter
+    contains an uppercase letter
+    contains a digit
+    only contains alphanumeric characters (note that '_' is not alphanumeric)
+*/
+// assign your RegExp to REGEXP
+const REGEXP = /^(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z])(?=\D*\d)[A-Za-z0-9]{6,}$/;
