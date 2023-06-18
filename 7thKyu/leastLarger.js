3c7969873901575252b0ea484f11af3462dfcc88
Łukasz Wargocki <lmwargocki@gmail.com>
2023-06-18 19:09:09 +0200

    write a function that, given an array and an integer representing an index, returns the index of an element that is least larger than array's element under the input index

---
+++ 7thKyu/leastLarger.js
@@ -0,0 +1,17 @@
+/* ask
+
+Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+Notes
+
+Multiple correct answers may be possible. In this case, return any one of them.
+The given index will be inside the given array.
+The given array will, therefore, never be empty.
+Example
+
+leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+*/
+function leastLarger(a,i) {
+  // return the index of minimum element of array of elements bigger than tested value
+  return a.indexOf(Math.min(...a.filter(element => element > a[i])));
+}
