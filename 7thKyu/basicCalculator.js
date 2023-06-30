198c2c9f073f12d79eccd41107250596a6de6c50
Lukasz Wargocki <lmwargocki@gmail.com>
2023-06-30 14:05:14 +0200

    write a function that, given three parameters - two operands as numbers and an operator as a string, returns the result of applying the operation to operands

---
+++ 7thKyu/basicCalculator.js
@@ -0,0 +1,22 @@
+/* Write a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null (throw an ArgumentException in C#).
+
+calculate(2,"+", 4); //Should return 6
+calculate(6,"-", 1.5); //Should return 4.5
+calculate(-4,"*", 8); //Should return -32
+calculate(49,"/", -7); //Should return -7
+calculate(8,"m", 2); //Should return null
+calculate(4,"/",0) //should return null
+
+Keep in mind, you cannot divide by zero. If an attempt to divide by zero is made, return null (throw an ArgumentException in C#)/(None in Python).
+*/
+function calculate(num1, operation, num2) {
+  // store functions in object to read from
+  const operators = {
+    "+": (a,b) => a + b,
+    "-": (a,b) => a - b,
+    "*": (a,b) => a * b,
+    "/": (a,b) => (b !== 0) ? a / b : null
+  };
+  // if property in object, apply proper method and return
+  return (operation in operators) ? operators[operation](num1, num2) : null;
+}
