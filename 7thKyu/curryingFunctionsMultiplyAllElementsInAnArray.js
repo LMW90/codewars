8a9e65ff86b0c933b67415ce93957bbbaec9db4e
Lukasz Wargocki <lmwargocki@gmail.com>
2023-09-12 14:22:10 +0200

    write a function that, given an array returns a function that returns a copy of an array multiplied by input number

---
+++ 7thKyu/curryingFunctionsMultiplyAllElementsInAnArray.js
@@ -0,0 +1,19 @@
+/* To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+Example:
+
+multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+
+You must not mutate the original array.
+*/
+function multiplyAll(arr) {
+  // return array multiplying function
+  return function (factor) {
+    // make a copy
+    let clone = arr.slice();
+    // return copy multiplied by factor
+    return clone.map((item, index) => clone[index] = item*factor);
+  }
+}
