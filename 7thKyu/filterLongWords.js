d8b7195049d952ac17cc366c23ab7f8e38b62026
Lukasz Wargocki <lmwargocki@gmail.com>
2023-10-06 17:23:35 +0200

    write a function that, given a sentence and an integer, returns an array of sentence's words longer than the integer

---
+++ 7thKyu/filterLongWords.js
@@ -0,0 +1,11 @@
+/* Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+Example:
+
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+*/
+function filterLongWords(sentence, n) {
+  // split string into words and keep only ones longer than n
+  return sentence.split(' ').filter(word => word.length > n);
+}
