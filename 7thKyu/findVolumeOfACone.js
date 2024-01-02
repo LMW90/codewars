496ceac1d98e53f825f9fe501ce818321c6871a5
Lukasz Wargocki <lmwargocki@gmail.com>
2024-01-02 19:10:41 +0100

    all done

---
+++ 7thKyu/findVolumeOfACone.js
@@ -0,0 +1,7 @@
+/* Find the volume of a cone whose radius and height are provided as parameters to the function volume. Use the value of PI provided by your language (for example: Math.PI in JS, math.pi in Python or Math::PI in Ruby) and round down the volume to an Interger.
+
+If you complete this kata and there are no issues, please remember to give it a ready vote and a difficulty rating. :)
+*/
+function volume(r, h) {
+  return Math.trunc(Math.PI * r * r * h / 3);
+}
